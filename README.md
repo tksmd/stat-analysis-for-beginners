479f69f68beeaf079f6b5cbdf9ed0aace1fab2b0
someda <someda@isenshi.com>
2015-06-03 02:13:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Setup
+
+Python 3.4.3 or later required. 
+
+If you use virtualenv, create virtualenv and activate it.
+
+    $ virtualenv --no-site-packages venv
+    $ source venv/bin/activate
+
+To install required package, run pip install as follows
+
+    $ pip install -r requirements.txt
+
+# Run scripts
+
+    $ ipython --matplotlib
+    In [1]: %run chapter/ch1_1.py
---
+++ chapter/ch1_1.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+
+import os
+import numpy as np
+
+from pandas import DataFrame, Series
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+data_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data")
+weights = Series(np.loadtxt(os.path.join(data_dir, "ch1_1.txt")))
+
+# p14
+min_weight = weights.min()
+max_weight = weights.max()
+data_range = max_weight - min_weight
+
+# Sturges' formula
+n = np.ceil(1 + np.log2(weights.size))
+
+# p15
+
+## frequency table
+cats = pd.cut(weights, n)
+counts = pd.value_counts(cats).sort_index()
+
+
+def to_int(s):
+    return np.ceil(float(s.strip()))
+
+
+class_values = [(to_int(s) + to_int(e)) // 2 for [s, e] in [v[1:-1].split(",") for v in counts.index]]
+df = DataFrame(counts.values, columns=["frequency"], index=class_values)
+
+sample_size = df["frequency"].sum()
+df["relative_frequency"] = df["frequency"] / sample_size
+df["cum_frequency"] = df["frequency"].cumsum()
+df["cum_relative_frequency"] = df["cum_frequency"] / sample_size
+
+## histogram
+ax = weights.hist(bins=n)
+ax.set_title("Fig 1.1.7 Histogram of Weight")
+ax.set_xlabel("Weight")
+ax.set_ylabel("Frequency")
+plt.show()
